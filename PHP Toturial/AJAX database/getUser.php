90279ff1550d08ae3f8bd44ff2478b24c021e9c1
nguyendinhkhanh98 <nguyendinhkhanh20061998@gmail.com>
2021-05-13 14:05:41 +0700

    getuser

---
+++ PHP Toturial/AJAX database/getUser.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <style>
+            table{
+                width: 100%;
+                border-collapse: collapse;
+            }
+            table, th, td{
+                border: 1px solid black;
+                padding: 5px;
+
+            }
+            th{
+                text-align: left;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+        
+    </body>
+</html>
